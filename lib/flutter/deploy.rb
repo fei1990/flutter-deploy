b94163103fe2401fa256d8c6576138aec1861dfd
wangfei <feiwang216830@sohu-inc.com>
2023-05-29 14:59:46 +0800

    git first commit

---
+++ lib/flutter/deploy.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "deploy/version"
+
+module Flutter
+  module Deploy
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/flutter/deploy_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Flutter::Deploy do
+  it "has a version number" do
+    expect(Flutter::Deploy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
